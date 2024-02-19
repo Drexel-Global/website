5b209caf1c474b18c408a6b410df57b8a00f3d92
Samuel Torres <rilla@Samuels-iMac.local>
2024-02-18 23:00:27 -0500

    eod update

--- drexel/src/app/assets/heroImages/heroImages.tsx
+++ drexel/src/app/assets/heroImages/heroImages.tsx
@@ -1,9 +1,11 @@
 export const heroImages: {
   money: string;
+  money2: string;
   bullish: string;
   background: string;
 } = {
   money: "drexel-finance-website/landing/twgl3zmhxz90kfquohwq",
+  money2: "drexel-finance-website/landing/rmh7wwmwldbjjktlo79l",
   bullish: "drexel-finance-website/landing/jwfpdemttjivqwwckhqi",
   background: "drexel-finance-website/landing/b392sqnl2r715zmxvmh4",
 };
