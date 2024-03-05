3b007df9b138ab2cfe18f1b7a1432af1bb1ac087
Samuel Torres <rilla@Samuels-iMac.local>
2024-03-05 15:44:45 -0500

    wrap up performance modal

--- drexel/src/app/components/contactModal/contactModal.tsx
+++ drexel/src/app/components/contactModal/contactModal.tsx
@@ -6,9 +6,19 @@ import ReCAPTCHA from "react-google-recaptcha";
 import axios from "axios";
 import { CldImage } from "next-cloudinary";
 import { motion } from "framer-motion";
+import dynamic from "next/dynamic";
 
 // components:
 import BallSpinner from "../loaders/ballSpinner";
+
+const Recaptcha = dynamic(() => import("../ctaButton/ctaButton"), {
+  ssr: false,
+  loading: () => (
+    <div>
+      <BallSpinner />
+    </div>
+  ),
+});
 
 const EmailRegex =
   /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/;
