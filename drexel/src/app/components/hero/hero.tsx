f0268ff37a05fb0f56b17e51553add7b4d387b1b
Samuel Torres <rilla@Samuels-iMac.local>
2024-03-06 14:49:25 -0500

    Add Yelp review widget

--- drexel/src/app/components/hero/hero.tsx
+++ drexel/src/app/components/hero/hero.tsx
@@ -183,6 +183,12 @@ export const Hero = () => {
             height={600}
             alt="bullish stock market animation"
           />
+          <iframe
+            src="https://afb93a38dca34e9389f3aea070402c25.elf.site"
+            width="100%"
+            height="1000"
+            frameborder="0"
+          ></iframe>
         </motion.div>
       </div>
       <div>
