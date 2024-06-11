f813e656fabef806b18a2956f94584535cbd5c4e
Samuel Torres <rilla@Samuels-iMac.local>
2024-06-11 17:12:30 -0400

    main page SEO metadata update

--- drexel/src/app/layout.tsx
+++ drexel/src/app/layout.tsx
@@ -32,6 +32,33 @@ export const metadata = {
       },
     ],
   },
+  twitter: {
+    card: "summary_large_image",
+    title: "Drexel Global Consulting Wealth Management by: Izhar Shefer",
+    description:
+      "Welcome to Drexel Global Consulting, your trusted partner for comprehensive wealth management solutions. Led by Managing Director Izhar Shefer, our team brings over 30 years of experience in crafting personalized investment strategies tailored to your goals and risk tolerance.",
+    creator: "Izhar Shefer",
+    images: [
+      {
+        url: "https://res.cloudinary.com/db09icibj/image/upload/v1718139107/drexel-finance-website/landing/dchrtnkjmrup5uvnlqk8.png",
+        width: 1200,
+        height: 630,
+      },
+    ],
+  },
+  robots: {
+    index: true,
+    follow: true,
+    nocache: true,
+    googleBot: {
+      index: true,
+      follow: false,
+      noimageindex: true,
+      "max-video-preview": -1,
+      "max-image-preview": "large",
+      "max-snippet": 500,
+    },
+  },
 };
 
 export const viewport: Viewport = {
