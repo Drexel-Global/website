e9b2ca912fb62380cd358178f0f23bfdcfb905d2
Samuel Torres <rilla@MacBook-Pro.local>
2024-10-20 12:17:47 -0400

    updates

--- drexel/src/app/sitemap.ts
+++ drexel/src/app/sitemap.ts
@@ -1,36 +1,36 @@
-import { MetadataRoute } from 'next'
- 
-export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
-    async function getData() {
-        const currentEnvUrls: string =
-          process.env.NEXT_PUBLIC_NODE_ENV === "development"
-            ? "http://localhost:1337/api/blog-articles?populate=*"
-            : `${process.env.NEXT_PUBLIC_BASE_URL}/api/blog-articles?populate=*`;
-      
-        const res = await fetch(currentEnvUrls);
-      
-        if (res.status !== 200) throw new Error("Failed to fetch data");
-      
-        return res.json();
-      }
+import { MetadataRoute } from "next";
 
-    const data = await getData();
-    const blogs = data?.data;
-    
-    const postUrls = blogs.map((blog: any) => ({
-        url: `${process.env.NEXT_PUBLIC_BASE_URL}blogs/${blog?.attributes?.slug}`,
-        lastModified: new Date(blog?.attributes?.updatedAt)
-    }))
+export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
+  async function getData() {
+    const currentEnvUrls: string =
+      process.env.NEXT_PUBLIC_NODE_ENV === "development"
+        ? "http://localhost:1337/api/blog-articles?populate=*"
+        : `${process.env.NEXT_PUBLIC_STRAPI_BASE_URL}/api/blog-articles?populate=*`;
+
+    const res = await fetch(currentEnvUrls);
+
+    if (res.status !== 200) throw new Error("Failed to fetch data");
+
+    return res.json();
+  }
+
+  const data = await getData();
+  const blogs = data?.data;
+
+  const postUrls = blogs.map((blog: any) => ({
+    url: `${process.env.NEXT_PUBLIC_STRAPI_BASE_URL}blogs/${blog?.attributes?.slug}`,
+    lastModified: new Date(blog?.attributes?.updatedAt),
+  }));
 
   return [
     {
-        url: `${process.env.NEXT_PUBLIC_BASE_URL}`,
-        lastModified: new Date(),
+      url: `${process.env.NEXT_PUBLIC_STRAPI_BASE_URL}`,
+      lastModified: new Date(),
     },
     {
-        url: `${process.env.NEXT_PUBLIC_BASE_URL}blogs`,
-        lastModified: new Date(),
+      url: `${process.env.NEXT_PUBLIC_STRAPI_BASE_URL}blogs`,
+      lastModified: new Date(),
     },
-    ...postUrls
-  ]
+    ...postUrls,
+  ];
 }
